9cb0156fede94f5f4b351f1a6994893f00ea4f43
RaintSun <2121321162@qq.com>
2026-09-10 16:20:46 +0800

    新增计分

--- snake.cpp
+++ snake.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <cstdlib>
 using namespace std;
 
 const int width = 20;
@@ -9,7 +10,7 @@ const int height = 20;
 int x, y, foodX, foodY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum Dir { STOP=0, LEFT, RIGHT, UP, DOWN };
+enum Dir { STOP = 0, LEFT, RIGHT, UP, DOWN };
 Dir dir;
 bool gameOver;
 
@@ -28,45 +29,47 @@ void Draw()
 {
     system("cls");
     // 上边框
-    for(int i=0; i<width+2; i++) cout << "#";
+    for (int i = 0; i < width + 2; i++) cout << "#";
     cout << endl;
 
-    for(int i=0; i<height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j=0; j<width; j++)
+        for (int j = 0; j < width; j++)
         {
-            if(j == 0) cout << "#";
-            if(i == y && j == x) cout << "O"; //蛇头
-            else if(i == foodY && j == foodX) cout << "F"; //食物
+            if (j == 0) cout << "#";
+            if (i == y && j == x)
+                cout << "O"; //蛇头
+            else if (i == foodY && j == foodX)
+                cout << "F"; //食物
             else
             {
                 bool print = false;
-                for(int k=0; k<nTail; k++)
+                for (int k = 0; k < nTail; k++)
                 {
-                    if(tailX[k]==j && tailY[k]==i)
+                    if (tailX[k] == j && tailY[k] == i)
                     {
                         cout << "o";
                         print = true;
                     }
                 }
-                if(!print) cout << " ";
+                if (!print) cout << " ";
             }
-            if(j == width-1) cout << "#";
+            if (j == width - 1) cout << "#";
         }
         cout << endl;
     }
 
     // 下边框
-    for(int i=0; i<width+2; i++) cout << "#";
+    for (int i = 0; i < width + 2; i++) cout << "#";
     cout << endl;
-    cout << "Score: " << score << endl;
+    cout << "当前分数: " << score << endl;
 }
 
 void Input()
 {
-    if(_kbhit())
+    if (_kbhit())
     {
-        switch(_getch())
+        switch (_getch())
         {
             case 'a': dir = LEFT; break;
             case 'd': dir = RIGHT; break;
@@ -85,7 +88,7 @@ void Logic()
     int prev2X, prev2Y;
     tailX[0] = x;
     tailY[0] = y;
-    for(int i=1; i<nTail; i++)
+    for (int i = 1; i < nTail; i++)
     {
         prev2X = tailX[i];
         prev2Y = tailY[i];
@@ -96,7 +99,7 @@ void Logic()
     }
 
     //移动蛇头
-    switch(dir)
+    switch (dir)
     {
         case LEFT: x--; break;
         case RIGHT: x++; break;
@@ -106,15 +109,15 @@ void Logic()
     }
 
     //撞墙
-    if(x<0 || x>=width || y<0 || y>=height) gameOver = true;
+    if (x < 0 || x >= width || y < 0 || y >= height) gameOver = true;
     //撞自己
-    for(int i=0; i<nTail; i++)
-        if(tailX[i]==x && tailY[i]==y) gameOver = true;
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y) gameOver = true;
 
-    //吃到食物
-    if(x == foodX && y == foodY)
+    //吃到食物：加分+加长蛇身
+    if (x == foodX && y == foodY)
     {
-        score +=10;
+        score += 10;
         foodX = rand() % width;
         foodY = rand() % height;
         nTail++;
@@ -124,12 +127,18 @@ void Logic()
 int main()
 {
     Setup();
-    while(!gameOver)
+    while (!gameOver)
     {
         Draw();
         Input();
         Logic();
-        Sleep(80);
+        // 根据分数动态调整延迟，分数越高速度越快
+        int sleepTime = max(20, 80 - score / 2);
+        Sleep(sleepTime);
     }
+    //游戏结束输出最终得分
+    cout << "====游戏结束====" << endl;
+    cout << "最终得分：" << score << endl;
+    system("pause");
     return 0;
 }
